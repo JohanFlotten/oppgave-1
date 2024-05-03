7659ff6138a0abe3df853cb42309d860d2bbf8fa
JohanFlotten <johanflotten@gmail.com>
2024-05-03 10:55:41 +0200

    oppgave 1-3

---
+++ main.py
@@ -0,0 +1,34 @@
+string1 = "hello im a little bit stupid "
+string1 += "there"
+
+print(string1)
+
+x = 5
+z = 10
+y = z 
+
+print(x+y)
+
+
+list= ["hello ", "there ", "i ", "like ", "runescape",]
+
+new = string1.split()
+print(new)
+
+for word in list:
+    print(word)
+
+
+# for i in range(101):
+    # print(i)
+
+tall1 = float(input("number 1: "))
+tall2 = float(input("number 2: "))
+print(tall1+tall2)
+
+name = input("what is your name? ")
+print("hello there " + name)
+
+games = ["runescape", "cs2", "apex legenes", "valorant", "valheim"]
+
+print(games[3])
